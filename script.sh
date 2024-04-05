d34cecaff3168bf798d889ccacbf86633f6e121a
Christopher Tarkaa <88291960+christarkaa@users.noreply.github.com>
2024-04-05 15:04:25 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-# Make a new directory for this project
-mkdir bulk
+# Make a new directory for this project and change to the directory
+mkdir bulk && cd '$_"
 
 # Download datasets
 fastq-dump --split-files SRR28420798
 
+# Make a new directory for quality control
+mkdir QC_Reports
+
 # Quality control using FastQC
-fastqc 
+fastqc SRR28420798_1.fastq SRR28420798_2.fastq -o QC_Reports
+
+# Trimming using Trimmomatic
