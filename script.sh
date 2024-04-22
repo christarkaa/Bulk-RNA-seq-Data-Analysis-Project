7ff208e7a7dc2773474d4579fa6876fec687d2ca
Christopher Tarkaa <88291960+christarkaa@users.noreply.github.com>
2024-04-22 10:25:30 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Make a new directory for this project and change to the directory
-mkdir bulk && cd '$_"
+mkdir bulk && cd $_
 
 # Download datasets
 fastq-dump --split-files SRR28420795
@@ -13,26 +13,26 @@ mkdir Mapping
 # Quality control using FastQC
 fastqc SRR28420795_1.fastq SRR28420795_2.fastq -o QC_Reports
 
-# Summarising the QC results
+# Summarizing the QC results
 multiqc .
 
 # Trimming using Trimmomatic
 java -jar ~/bin/trimmomatic/trimmomatic-0.39/trimmomatic-0.39.jar \
   PE -phred33 \
-   SRR28420795_1.fastq SRR28420795_2.fastq \
+  SRR28420795_1.fastq SRR28420795_2.fastq \
   paired1.fastq unpaired1.fastq \
   paired2.fastq unpaired2.fastq \
   TRAILING:20 MINLEN:50
 
-# Alignment with hisat2
+# Alignment with HISAT2
 hisat2 -x genome -1 paired1.fastq -2 paired2.fastq -S Mapping/SRR28420795.sam
 
-# Convert to a bam file
+# Convert to a BAM file
 samtools view -@ 20 -S -b Mapping/SRR28420795.sam > Mapping/SRR28420795.bam
 
-# Sort the bam file
+# Sort the BAM file
 samtools sort -@ 32 -o Mapping/SRR28420795.sorted.bam Mapping/SRR28420795.bam
 
-# Quatification of counts using featureCount 
-featureCounts -T 8 -t exon -a annotations.gtf -o counts.txt SRR28420795_sorted.bam
+# Quantification of counts using featureCounts
+featureCounts -T 8 -t exon -a annotations.gtf -o counts.txt Mapping/SRR28420795.sorted.bam
 
