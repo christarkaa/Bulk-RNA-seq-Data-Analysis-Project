d37e37ec23a180cabd81b86bdc72286b187d1f37
Christopher Tarkaa <88291960+christarkaa@users.noreply.github.com>
2024-04-23 15:39:12 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -31,5 +31,5 @@ samtools view -@ 20 -S -b Mapping/SRR28420795.sam > Mapping/SRR28420795.bam
 samtools sort -@ 32 -o Mapping/SRR28420795.sorted.bam Mapping/SRR28420795.bam
 
 # Quantification of counts using featureCounts
-featureCounts -T 8 -t exon -g gene_id -a annotations.gtf -o counts_SRR28420795.txt Mapping/SRR28420795.sorted.bam
+featureCounts -T 8 -t exon -g gene_id -a annotations.gtf -o counts.SRR28420795.txt Mapping/SRR28420795.sorted.bam
 
